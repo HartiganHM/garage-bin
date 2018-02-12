208bd3d0f27ed71d4879b8ee5dbdaa641eb3a69c
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-02-12 11:25:55 -0700

    Write test for GET item by id

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -62,4 +62,32 @@ describe('API Routes', () => {
         });
     });
   });
+
+  describe('GET item by id', () => {
+    beforeEach(done => {
+      knex.seed.run().then(() => {
+        done();
+      });
+    });
+
+    it('Should get an item by id', () => {
+      return chai
+        .request(server)
+        .get('/api/v1/garage-bin/1')
+        .then(response => {
+          response.should.have.status(200);
+          response.should.be.json;
+          response.body.should.be.a('array');
+          response.body.length.should.equal(1);
+          response.body[0].should.have.property('id');
+          response.body[0].id.should.equal(1);
+          response.body[0].should.have.property('name');
+          response.body[0].should.have.property('reason');
+          response.body[0].should.have.property('cleanliness');
+        })
+        .catch(error => {
+          throw error;
+        });
+    });
+  });
 });
