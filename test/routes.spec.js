a31e7bd6bad56716c91b0a1f9e2511d16b451187
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-02-12 11:03:07 -0700

    Write successful client get request test

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -10,14 +10,19 @@ chai.use(chaiHttp);
 
 describe('Client Routes', () => {
   it('Should return the homepage', () => {
-    
+    return chai
+      .request(server)
+      .get('/')
+      .then(respone => {
+        response.should.have.status(200);
+        response.should.be.html;
+      })
+      .catch(error => {
+        throw error;
+      });
   });
 
-  it('Should return a 404 error for a route that does not exists', () => {
-    
-  })
+  it('Should return a 404 error for a route that does not exists', () => {});
 });
 
-describte('API Routes', () => {
-
-})
+describte('API Routes', () => {});
