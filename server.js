4aa3de7d7165e2f8ef00e885cf5d95d6d353d741
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-02-12 09:46:46 -0700

    Set port for app and locals title

--- server.js
+++ server.js
@@ -5,4 +5,8 @@ const bodyParser = require('body-parser');
 const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration);
+const port = process.env.PORT || 3000;
 
+app.locals.title = 'Garage Bin';
+
+app.set('port', port);
