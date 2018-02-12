9c90f401dbf1ad8248586977450ced9bd48203f0
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-02-12 09:19:00 -0700

    Configure migrations and seeds for development environment

--- knexfile.js
+++ knexfile.js
@@ -3,6 +3,12 @@ module.exports = {
   development: {
     client: 'pg',
     connection: 'postgres://localhost/garbage_bin',
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/dev'
+    },
     useNullAsDefault: true
   }
 };
