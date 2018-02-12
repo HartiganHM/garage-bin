7c585cd5180b352ec41c37bfd8c873f29d393581
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-02-12 11:00:30 -0700

    Mock out client side tests

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -6,3 +6,18 @@ const chaiHttp = require('chai-http');
 const server = require('../server');
 const knex = require('../db/knex');
 
+chai.use(chaiHttp);
+
+describe('Client Routes', () => {
+  it('Should return the homepage', () => {
+    
+  });
+
+  it('Should return a 404 error for a route that does not exists', () => {
+    
+  })
+});
+
+describte('API Routes', () => {
+
+})
