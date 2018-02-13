ef2dabdd0999d49c4c31bd11b3eecc7a92f8cf4c
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-02-12 18:54:01 -0700

    Cors fix

--- server.js
+++ server.js
@@ -9,10 +9,9 @@ const port = process.env.PORT || 3000;
 
 const accessControlAllowOrigin = (request, response, next) => {
   response.header('Access-Control-Allow-Origin', '*');
-  response.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   response.header(
     'Access-Control-Allow-Headers',
-    'Content-Type, Authorization, Content-Length, X-Requested-With, x-token'
+    'Origin, X-Requested-With, Content-Type, Accept'
   );
   next();
 };
